1ecdd40df5127c97dd0ca9bb251f3c547349e40f
rodolfo-22 <00082421@uca.edu.sv>
2022-11-17 19:23:13 -0600

    Adding_about_page_full_response

--- Client/src/Components/VisionComponents/VisionPage.jsx
+++ Client/src/Components/VisionComponents/VisionPage.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import '../MisionComponents/Mision.css'
 
 const VisionPage = () => {
   return (
-    <h1>
-      pagina que llevara la vision de cc
-    </h1>
+    <>
+        <div className='contenedores'>
+
+        <div className='contenedor1'>
+          <p>Vision</p>
+        </div>
+        <div className='contenedor2'>
+          <div >
+            <p>
+            Ser una instancia modelo reconocida en la promoción y acompañamiento 
+            de un servicio social estudiantil que contribuya en la formación 
+            de profesionales con sensibilidad social comprometidos con la construcción
+            de una sociedad que promueva la dignidad humana.
+            </p>
+            </div>
+          </div>
+        
+        </div>
+    </>
   )
 }
 
