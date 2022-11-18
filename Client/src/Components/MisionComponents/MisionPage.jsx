ccb1157417925519e6568aae10f90dd3effceb54
rodolfo-22 <00082421@uca.edu.sv>
2022-11-17 19:05:27 -0600

    Adding_Mision_page_full_response

--- Client/src/Components/MisionComponents/MisionPage.jsx
+++ Client/src/Components/MisionComponents/MisionPage.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import './Mision.css'
 
 const MisionPage = () => {
   return (
-    <h1>
-      pagina que llevara la mision de cc
-    </h1>
+    <>
+      <div className='contenedores'>
+
+        <div className='contenedor1'>
+          <p>Mision</p>
+        </div>
+        <div className='contenedor2'>
+          <div >
+            <p>Velar por la realización de un servicio social estudiantil, 
+                basado en la solidaridad, trabajo en equipo, responsabilidad y honradez, 
+                de preferencia dirigido hacia los sectores más pobres de nuestro país 
+                con apoyo y participación de la comunidad universitaria.
+            </p>
+            </div>
+        </div>
+        
+    </div>
+    </>
   )
 }
 
-export default MisionPage
+export default MisionPage;
